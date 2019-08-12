3cdd119a66b692c7818f82191a35e5f1cc2fa377
Mirko Bronzi <m.bronzi@gmail.com>
2019-08-12 14:09:14 -0400

    now providing to_complete and solution

--- mask_ipynb_answers.py
+++ mask_ipynb_answers.py
@@ -10,7 +10,7 @@ SPACES_AT_LINE_START = re.compile('" *')
 REPLACEMENT_TEXT = '... # To complete.'
 
 
-def mask_ipynb(in_stream, out_stream):
+def mask_ipynb(in_stream, to_complete_stream, solution_stream):
     in_answer_block = False
     next_line_answer = False
     blocks = 0
@@ -22,7 +22,7 @@ def mask_ipynb(in_stream, out_stream):
                                  ' answer block')
             in_answer_block = True
             spaces = SPACES_AT_LINE_START.findall(line)
-            out_stream.write('    ' + spaces[0] + '...\\n",\n')
+            to_complete_stream.write('    ' + spaces[0] + REPLACEMENT_TEXT + '\\n",\n')
         elif RE_END_BLOCK.match(line):
             if not in_answer_block:
                 raise ValueError('end answer block when not inside an'
@@ -41,13 +41,15 @@ def mask_ipynb(in_stream, out_stream):
                 ending = '\\n",'
             else:
                 ending = '"'
-            out_stream.write(part_to_keep[0] + '... ' + ending + '\n')
+            to_complete_stream.write(part_to_keep[0] + REPLACEMENT_TEXT + ending + '\n')
+            solution_stream.write(line)
             one_line += 1
             next_line_answer = False
-        elif in_answer_block:  # just skip this line
-            pass
+        elif in_answer_block:  # just skip this line for the to_complete
+            solution_stream.write(line)
         else:
-            out_stream.write(line)
+            to_complete_stream.write(line)
+            solution_stream.write(line)
     if in_answer_block:
         raise ValueError('completed without closing the answer block')
     print('substituted {} blocks - and {} lines'.format(blocks, one_line))
@@ -55,18 +57,22 @@ def mask_ipynb(in_stream, out_stream):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', help='path to input ipynb',
+    parser.add_argument('--input_file', help='path to input ipynb (source)',
                         required=True)
-    parser.add_argument('--output_file', help='path to output ipynb',
+    parser.add_argument('--to_complete_file', help='path to output ipynb (to complete)',
+                        required=True)
+    parser.add_argument('--solution_file', help='path to output ipynb (solution)',
                         required=True)
     args = parser.parse_args()
 
-    print('masking file {} - writing output into {}'.format(
-        args.input_file, args.output_file))
+    print('masking file {} - writing output (to complete) into {} - writing'
+          'output (solution) into {}'.format(
+        args.input_file, args.to_complete_file, args.solution_file))
 
     with open(args.input_file, 'r', encoding='utf8') as in_stream:
-        with open(args.output_file, 'w', encoding='utf8') as out_stream:
-            mask_ipynb(in_stream, out_stream)
+        with open(args.to_complete_file, 'w', encoding='utf8') as to_complete_stream:
+            with open(args.solution_file, 'w', encoding='utf8') as solution_stream:
+                mask_ipynb(in_stream, to_complete_stream, solution_stream)
 
 
 
