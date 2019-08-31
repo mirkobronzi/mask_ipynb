5d0a18480ef3e226cf76be7d33561e447183696c
Mirko Bronzi <m.bronzi@gmail.com>
2019-08-31 11:18:35 -0400

    fixed typo

--- example/run.sh
+++ example/run.sh
@@ -1,1 +1,1 @@
-python ../mask_ipynb_answers.py --input_file test_check.ipynb --to_complete_file test_to_complete.ipynb --solution_file test_solution.ipyn
+python ../mask_ipynb_answers.py --input_file test_check.ipynb --to_complete_file test_to_complete.ipynb --solution_file test_solution.ipynb
